87a4662d553d8bee27e8b4899c1e2fcd55fe8a1c
hromml <mrkhramkov@gmail.com>
2019-04-27 21:11:09 +0400

    first commit

---
+++ tests/simple_test.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from requests import get
+import pytest
+import os
+
+class TestOpenSite():
+
+    @pytest.fixture()
+    def test_setup(self):
+        global driver
+        google_driver_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../resources/drivers/chromedriver.exe")
+        driver = webdriver.Chrome(executable_path=google_driver_path)
+        driver.implicitly_wait(10)
+        driver.maximize_window()
+        yield
+        driver.close()
+        driver.quit()
+
+    def test_open_site(self, test_setup):
+        searchText = 'Атол-Онлайн'
+        driver.get('https://www.google.ru')
+        driver.find_element_by_name('q').send_keys(searchText.decode('utf-8'))
+        driver.find_element_by_name('q').send_keys(Keys.RETURN)
+        firstSite = driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div[1]/a').get_attribute('href')
+        response = get(firstSite)
+        code = response.status_code
+        assert code == 200
